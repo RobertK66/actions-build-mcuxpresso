0a9a1be0181289849d8e2f22f8412e1b579dcc75
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-13 11:00:20 +0100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 echo "Script called with args: '$*' "
 echo "************************ /usr/local/mcuxpressoide ******************"
-ls -Ral /usr/local/mcuxpressoide
-cat /usr/local/mcuxpressoide/Readme.txt
+ls -R /usr/local/mcuxpressoide
+ls -R /usr/local/mcuxpressoide-11.4.0_6237
 
+MY_IDE_PATH=/usr/local/mcuxpressoide-11.4.0_6237
+MY_TOOLCHAIN_PATH=$IDE_PATH/ide/tools/bin
+MY_IDE=$IDE_PATH/ide/mcuxpressoidec
+
+echo "$MY_IDE_PATH"
+echo "$MY_TOOLCHAIN_PATH"
+echo "$MY_IDE"
+
+echo "$PATH"
+PATH=$PATH;$TOOLCHAIN_PATH
+
+@REM -build on project: normal build, does a (re-)build of all targets (Release, Debug): the fact that it does a full build sounds lika an issue in Eclipse CDT 9.4.3?
+"$IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "$GITHUB_WORKSPACE" -build $1

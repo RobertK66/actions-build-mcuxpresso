07251a8debe75585f68c5365720867b7df1a9b8b
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-13 11:36:42 +0100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -3,16 +3,13 @@ echo "Script called with args: '$*' "
 MY_IDE_PATH=/usr/local/mcuxpressoide-11.4.0_6237
 MY_TOOLCHAIN_PATH=$MY_IDE_PATH/ide/tools/bin
 MY_IDE_CMD=$MY_IDE_PATH/ide/mcuxpressoide
-
-echo "MY_IDE_PATH: '$MY_IDE_PATH'"
-echo "MY_TOOLCHAIN_PATH: '$MY_TOOLCHAIN_PATH'"
-echo "MY_IDE_CMD: $MY_IDE_CMD"
-
-echo "vorher: $PATH"
 export PATH=$PATH:$MY_TOOLCHAIN_PATH
-echo "nachher: $PATH"
+
+ls -R /github/workspace
+
+
 
 echo "executing: '$MY_IDE_CMD'"
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data '$GITHUB_WORKSPACE' -build $1
+$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data $GITHUB_WORKSPACE/obc_1769_core -build $1
 
 echo "finished"

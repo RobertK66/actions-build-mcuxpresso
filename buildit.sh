146220a401f5f295cd2b3e464d326a0dafcfcd4f
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-13 11:12:30 +0100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 echo "Script called with args: '$*' "
-echo "************************ /usr/local/mcuxpressoide ******************"
-ls -R /usr/local/mcuxpressoide
-ls -R /usr/local/mcuxpressoide-11.4.0_6237
-
 MY_IDE_PATH=/usr/local/mcuxpressoide-11.4.0_6237
 MY_TOOLCHAIN_PATH=$IDE_PATH/ide/tools/bin
-MY_IDE=$IDE_PATH/ide/mcuxpressoidec
+MY_IDE_CMD=$IDE_PATH/ide/mcuxpressoide
 
-echo "$MY_IDE_PATH"
-echo "$MY_TOOLCHAIN_PATH"
-echo "$MY_IDE"
+echo "MY_IDE_PATH: '$MY_IDE_PATH'"
+echo "MY_TOOLCHAIN_PATH: '$MY_TOOLCHAIN_PATH'"
+echo "MY_IDE_CMD: $MY_IDE_CMD"
 
-echo "$PATH"
+echo "vorher: $PATH"
 PATH=$PATH;$TOOLCHAIN_PATH
+echo "nachher: $PATH"
 
-@REM -build on project: normal build, does a (re-)build of all targets (Release, Debug): the fact that it does a full build sounds lika an issue in Eclipse CDT 9.4.3?
-"$IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "$GITHUB_WORKSPACE" -build $1
+echo "executing $MY_IDE_CMD"
+$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "$GITHUB_WORKSPACE" -build $1
+
+echo "finished"

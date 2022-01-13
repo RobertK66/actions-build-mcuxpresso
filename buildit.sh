94ed64b135f50f719cadf1558421c6e511f0e4fa
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-13 12:39:38 +0100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -5,7 +5,7 @@ MY_TOOLCHAIN_PATH=$MY_IDE_PATH/ide/tools/bin
 MY_IDE_CMD=$MY_IDE_PATH/ide/mcuxpressoide
 export PATH=$PATH:$MY_TOOLCHAIN_PATH
 
-ls -R /github/workspace
+ls -Ral /github/workspace
 
 
 

fdba05f3ce9346fc0e2f32e74e2dd1a0537f376c
RobertK66 <RobertK66@users.noreply.github.com>
2023-01-16 09:07:54 +0100

    Update buildit.sh
    adapt build path

--- buildit.sh
+++ buildit.sh
@@ -5,17 +5,16 @@ MY_TOOLCHAIN_PATH=$MY_IDE_PATH/ide/tools/bin
 MY_IDE_CMD=$MY_IDE_PATH/ide/mcuxpressoide
 export PATH=$PATH:$MY_TOOLCHAIN_PATH
 
-#ls -Ral /github/workspace
-
+ls -Ral /github/workspace
 
 
 echo "executing: '$MY_IDE_CMD'"
 
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $GITHUB_WORKSPACE -import $GITHUB_WORKSPACE/ado-chip-175x-6x/ado_chip_175x_6x
+$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $GITHUB_WORKSPACE -import $GITHUB_WORKSPACE/ado_chip_175x_6x
 $MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ado_chip_175x_6x/TestConfig_2 
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ClimbObc/Debug 
+$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ClimbObc/ClimbObc/Debug 
 
-ls -al /github/workspace/Debug
+ls -al /github/workspace/ClimbObc/Debug
 ls -al Debug
 
 

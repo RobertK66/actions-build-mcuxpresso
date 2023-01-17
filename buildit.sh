c2054227574613990cad952379b0115e0f7a586e
RobertK66 <RobertK66@users.noreply.github.com>
2023-01-17 10:25:53 +0100

    Update buildit.sh
    add output as param

--- buildit.sh
+++ buildit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo "Script called with args: '$*' "
 
+#echo "Script called with args: '$*' "
 projects="${1}"
 configs="${2}"
+resultpath="${3}"
 
 MY_IDE_PATH=/usr/local/mcuxpressoide-11.4.0_6237
 MY_TOOLCHAIN_PATH=$MY_IDE_PATH/ide/tools/bin
@@ -10,28 +11,21 @@ MY_IDE_CMD=$MY_IDE_PATH/ide/mcuxpressoide
 export PATH=$PATH:$MY_TOOLCHAIN_PATH
 
 #ls -Ral /github/workspace
-
-
 #echo "executing: '$MY_IDE_CMD'"
 
 for projpath in $projects; do
-    echo "proj: '$projpath'"
+    echo "import project: '$projpath'"
     $MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $projpath
 done
 
 for buildit in $configs; do
-    echo "cfg: '$buildit'"
+    echo "build config: '$buildit'"
     $MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build $buildit
 done
 
-
-
-#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $GITHUB_WORKSPACE/ClimbObc -import $GITHUB_WORKSPACE/ado_chip_175x_6x
-#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ado_chip_175x_6x/TestConfig_2 
-#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ClimbObc/Debug 
-
 pwd
-ls -al /github/workspace/ClimbObc/Debug
-ls -al ClimbObc/Debug
+#ls -al /github/workspace/ClimbObc/Debug
+ls -al $resultpath
 
-echo "::set-output name=axf::ClimbObc/Debug/ClimbObc.axf"
+#echo "::set-output name=axf::ClimbObc/Debug/ClimbObc.axf"
+echo "axf=ClimbObc/Debug/ClimbObc.axf" >> $GITHUB_OUTPUT

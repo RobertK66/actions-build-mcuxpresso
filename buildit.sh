64a35b2c84c70c2d9a48e533288feaebc3669189
RobertK66 <RobertK66@users.noreply.github.com>
2023-01-17 10:14:45 +0100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -4,14 +4,6 @@ echo "Script called with args: '$*' "
 projects="${1}"
 configs="${2}"
 
-for projpath in $projects; do
-    echo "proj: '$projpath'"
-done
-
-for buildit in $configs; do
-    echo "cfg: '$buildit'"
-done
-
 MY_IDE_PATH=/usr/local/mcuxpressoide-11.4.0_6237
 MY_TOOLCHAIN_PATH=$MY_IDE_PATH/ide/tools/bin
 MY_IDE_CMD=$MY_IDE_PATH/ide/mcuxpressoide
@@ -20,11 +12,23 @@ export PATH=$PATH:$MY_TOOLCHAIN_PATH
 #ls -Ral /github/workspace
 
 
-echo "executing: '$MY_IDE_CMD'"
+#echo "executing: '$MY_IDE_CMD'"
 
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $GITHUB_WORKSPACE/ClimbObc -import $GITHUB_WORKSPACE/ado_chip_175x_6x
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ado_chip_175x_6x/TestConfig_2 
-$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ClimbObc/Debug 
+for projpath in $projects; do
+    echo "proj: '$projpath'"
+    $MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $projpath
+done
+
+for buildit in $configs; do
+    echo "cfg: '$buildit'"
+    $MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build $buildit
+done
+
+
+
+#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $GITHUB_WORKSPACE/ClimbObc -import $GITHUB_WORKSPACE/ado_chip_175x_6x
+#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ado_chip_175x_6x/TestConfig_2 
+#$MY_IDE_CMD -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build ClimbObc/Debug 
 
 pwd
 ls -al /github/workspace/ClimbObc/Debug
